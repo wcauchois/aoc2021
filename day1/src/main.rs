9139e9190318c1faaa5ab25b1d86c1cffc66859a
Bill Cauchois <wcauchois@gmail.com>
2021-12-01 11:47:10 -0500

    part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,15 +4,27 @@ use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let lines = read_lines("input_part1.txt")?;
-    let nums = lines.into_iter().map(|l| l.parse::<i32>()).collect::<Result<Vec<i32>, _>>()?;
+    let nums = lines
+        .into_iter()
+        .map(|l| l.parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()?;
     let mut num_increases = 0;
-    for subslice in nums.into_iter().as_slice().windows(2) {
-        match subslice {
-            [prev, next] => {
-                if next > prev {
+    for item in nums
+        .into_iter()
+        .as_slice()
+        .windows(3)
+        .collect::<Vec<_>>()
+        .as_slice()
+        .windows(2)
+    {
+        match item {
+            [fst, snd] => {
+                let fst_sum = fst.iter().sum::<i32>();
+                let snd_sum = snd.iter().sum::<i32>();
+                if snd_sum > fst_sum {
                     num_increases += 1;
                 }
-            },
+            }
             _ => panic!(),
         }
     }
@@ -25,5 +37,7 @@ where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    io::BufReader::new(file).lines().collect::<Result<Vec<String>, _>>()
+    io::BufReader::new(file)
+        .lines()
+        .collect::<Result<Vec<String>, _>>()
 }
